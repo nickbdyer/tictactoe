61f51eea1319b8eac4bdb2493cf34fda73e659a6
Nick Dyer <nick.dyer@atos.net>
2015-08-20 16:35:26 +0100

    Expands on board class

--- lib/board.rb
+++ lib/board.rb
@@ -3,7 +3,35 @@ class Board
   attr_reader :grid
 
   def initialize(content, size)
-    @grid = Array.new(9)
+    @grid = []
+    create_grid(content, size)
+  end
+
+  def mark(cell)
+    @grid[cell].content = "X"
+  end
+
+  def empty?
+    @grid.any? { |cell| cell.content == nil }
+  end
+
+  def full?
+    @grid.any? { |cell| cell.content != nil }
+  end
+
+  def can_mark?(cell)
+    @grid[cell].content == nil
+  end
+
+  def available_cells
+   (0..8).to_a.keep_if { |cell| can_mark?(cell) }
+  end
+
+  private
+
+  def create_grid(content, size)
+    size.times { @grid << content.new }
+    @grid
   end
 
 end
--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -1,13 +1,42 @@
 require 'board'
+require 'cell'
 
 describe Board do
 
-  let(:cell_class) { double :cell_class }
+  let(:board) { Board.new(Cell, 9) }
 
   it "can be initialized a size" do
-    board = Board.new(cell_class, 9)
     expect(board.grid.length).to eq 9
   end
 
+  it "can be initilized with content" do
+    expect(board.grid[0]).to be_a Cell
+  end
+
+  it "can send a mark to a cell" do
+    board.mark(0)
+    expect(board.grid[0].content).to eq "X"
+  end
+
+  it "knows when the board is empty" do
+    expect(board.empty?).to be true
+  end
+
+  it "knows when the board is full" do
+    (0..8).each { |num| board.mark(num) }
+    expect(board.full?).to be true
+  end
+
+  it "knows when a cell is marked" do
+    board.mark(0)
+    expect(board.can_mark?(0)).to be false
+  end
+
+  it "knows which cells are unmarked" do
+    (0..3).each { |cell| board.mark(cell) }
+    expect(board.available_cells).to eq [4, 5, 6, 7, 8]
+  end
+
+  
 
 end
