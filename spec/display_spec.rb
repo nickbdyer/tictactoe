e826c81a10181e6bce2418f7d6a5293ef717091b
Nick Dyer <nbdyer@gmail.com>
2015-08-22 09:27:01 +0100

    Display test passing, although formatting for stdout is a bit heath robinson

--- spec/display_spec.rb
+++ spec/display_spec.rb
@@ -4,13 +4,16 @@ describe Display do
 
   let(:game) { double :game, grid: [ nil, "X", "X", "O", nil, "O", "X", "O", "X"] }
 
-  it "can show the board" do
+  it "can show the board (test works, but is a bit fudged)" do
     display = Display.new
-    expect(display.show(game)).to eq "   | X | X
-    ---|---|---
-     O |   | O
-     ---|---|---
-        X | O | X"
+  expect{display.show(game)}.to output(
+"   | X | X 
+---|---|---
+ O |   | O 
+---|---|---
+ X | O | X 
+").to_stdout
   end
 
 end
+
