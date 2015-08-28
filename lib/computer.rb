8b5c0d1fb21a4026b97f518274a0479faa513e3b
Nick Dyer <nick.dyer@atos.net>
2015-08-28 12:31:12 +0100

    Adds engine into computer for access to game elements

--- lib/computer.rb
+++ lib/computer.rb
@@ -1,21 +1,21 @@
 class Computer
 
-  attr_accessor :symbol
+  attr_accessor :symbol, :engine
 
   def opponent_symbol
     @symbol == "X" ? "O" : "X"
   end
 
-  def mark(cell, engine)
-    engine.process_mark(cell)
+  def mark(cell)
+   @engine.process_mark(cell)
   end
 
-  def choose_move(board, engine)
-    chosen_cell = minimax(board)
-    mark(chosen_cell, engine)
+  def choose_move
+    chosen_cell = minimax(@engine.game.board)
+    mark(chosen_cell)
   end
 
-  def minimax(node, depth = 0, maximizingPlayer = true)
+  def minimax(node, depth = 6, maximizingPlayer = true)
     return score(node) if depth == 0 || board.full? || board.has_a_winner?
     if maximizingPlayer
       bestValue = -10
--- spec/computer_spec.rb
+++ spec/computer_spec.rb
@@ -9,37 +9,45 @@ describe Computer do
   let(:opponent_win_board) { double :board, winner: "O" }
   let(:draw_board) { double :board, full?: true, winner: false }
 
-  it "can have a symbol" do
+  def setup_computer
+    computer.engine = engine
     computer.symbol = "X"
+  end
+
+  before do
+    setup_computer
+  end
+
+  it "can have an engine" do
+    expect(computer.engine).to eq engine
+  end
+
+  it "can have a symbol" do
     expect(computer.symbol).to eq "X"
   end
 
   it "can mark the board" do
     expect(engine).to receive(:process_mark).with(3)
-    computer.mark(3, engine)
+    computer.mark(3)
   end
 
   it "will choose center if playing first" do
-    expect(computer.choose_move(empty_board, engine)).to eq 5
+    expect(computer.choose_move).to eq 5
   end
 
   it "knows what the opponents symbol is" do
-    computer.symbol = "X"
     expect(computer.opponent_symbol).to eq "O"
   end
 
   it "will score a board with 10 if it wins" do
-    computer.symbol = "X"
     expect(computer.score(comp_win_board)).to eq 10
   end
 
   it "will score a board with -10 if it loses" do
-    computer.symbol = "X"
     expect(computer.score(opponent_win_board)).to eq -10
   end
 
   it "will score a board with 0 if it is a draw" do
-    computer.symbol = "X"
     expect(computer.score(draw_board)).to eq 0
   end
 
