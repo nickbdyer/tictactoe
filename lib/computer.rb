517ace611c29dae8cf85719b42ebb46be01b0a11
Nick Dyer <nbdyer@gmail.com>
2015-09-08 18:43:59 +0100

    Implemented negamax with alpha beta

--- lib/computer.rb
+++ lib/computer.rb
@@ -4,6 +4,8 @@ class Computer
 
   def initialize
     @scored_moves = {}
+    @scored_moves_ab = {}
+    @scored_moves_neg_ab = {}
   end
 
   def opponent_symbol
@@ -16,8 +18,25 @@ class Computer
 
   def choose_move
     return 4 if @engine.game.board.empty?
-    alphabeta(@engine.game.board)
-    @scored_moves.max_by{|k,v| v}.first
+    negamax(@engine.game.board)
+    @scored_moves_neg_ab.max_by{|k,v| v}.first
+  end
+
+  def negamax(board, depth = 0, α = -10, β = 10, color = 1)
+    return color * score(board, depth) if board.full? || board.has_a_winner?
+    bestValue = -10
+    board.available_cells.each do |cell|
+      color > 0 ? board.mark(cell, @symbol) : board.mark(cell, opponent_symbol)
+      val = -negamax(board, depth + 1, -β, -α, -color)
+      board.grid[cell].content = nil
+      bestValue = [bestValue, val].max
+      @scored_moves_neg_ab[cell] = bestValue if depth == 0
+      α = [α, bestValue].max 
+      if α >= β
+        break
+      end
+    end
+    bestValue
   end
 
   def alphabeta(board, ai_turn = true, depth = 0, α = -10, β = 10)
@@ -28,7 +47,7 @@ class Computer
       val = ai_turn ? alphabeta(board, false, depth + 1, α, β) : alphabeta(board, true, depth + 1, α, β)
       board.grid[cell].content = nil
       bestValue = ai_turn ? [bestValue, val].max : [bestValue, val].min
-      @scored_moves[cell] = bestValue if depth == 0
+      @scored_moves_ab[cell] = bestValue if depth == 0
       ai_turn ? α = [α, bestValue].max : β = [β, bestValue].min
       if α >= β
         break
@@ -42,5 +61,40 @@ class Computer
     return (-10.0 / depth) if board.winner == opponent_symbol
     return 0 if board.full?
   end
+
+  private 
+
+  def minimax(board, depth = 0, alpha = -10, beta = 10, maximizingPlayer = true)
+    return score(board, depth) if board.full? || board.has_a_winner?
+    if maximizingPlayer
+      bestValue = -10
+      board.available_cells.each do |cell|
+        board.mark(cell, @symbol)
+        val = minimax(board, depth + 1, alpha, beta, false)
+        board.grid[cell].content = nil
+        bestValue = [bestValue, val].max
+        @scored_moves[cell] = bestValue if depth == 0
+        alpha = [alpha, bestValue].max
+        if alpha >= beta
+          break
+        end
+      end
+    else
+      bestValue = 10
+      board.available_cells.each do |cell|
+        board.mark(cell, opponent_symbol)
+        val = minimax(board, depth + 1, alpha, beta, true)
+        board.grid[cell].content = nil
+        bestValue = [bestValue, val].min
+        @scored_moves[cell] = bestValue if depth == 0
+        beta = [beta, bestValue].min
+        if alpha >= beta
+          break
+        end
+      end
+    end
+    bestValue
+  end
+
 
 end
