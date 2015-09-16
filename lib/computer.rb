6e15623a1569e5cca43d56258efcfe77d2683625
Nick Dyer <nick.dyer@atos.net>
2015-09-16 11:17:36 +0100

    Hides instance variables in computer class

--- lib/computer.rb
+++ lib/computer.rb
@@ -1,35 +1,36 @@
 class Computer
 
   attr_accessor :symbol, :engine, :name
+  attr_reader :scored_moves
 
   def initialize
     @scored_moves = {}
   end
 
   def opponent_symbol
-    @symbol == "X" ? "O" : "X"
+    symbol == "X" ? "O" : "X"
   end
 
   def mark(cell)
-    @engine.process_mark(cell)
+    engine.process_mark(cell)
   end
 
   def choose_move
-    return 4 if @engine.game.board.empty?
+    return 4 if engine.game.board.empty?
     @scored_moves = {}
-    negamax(@engine.game.board)
-    @scored_moves.max_by(&:last).first
+    negamax(engine.game.board)
+    scored_moves.max_by(&:last).first
   end
 
   def negamax(board, depth = 0, α = -10, β = 10, color = 1)
     return color * score(board, depth) if board.full? || board.has_a_winner?
     bestValue = -10
     board.available_cells.each do |cell|
-      color == 1 ? board.mark(cell, @symbol) : board.mark(cell, opponent_symbol)
+      color == 1 ? board.mark(cell, symbol) : board.mark(cell, opponent_symbol)
       val = -negamax(board, depth + 1, -β, -α, -color)
       board.grid[cell].content = nil
       bestValue = [bestValue, val].max
-      @scored_moves[cell] = bestValue if depth == 0
+      scored_moves[cell] = bestValue if depth == 0
       α = [α, bestValue].max 
       if α >= β
         break
@@ -39,7 +40,7 @@ class Computer
   end
 
   def score(board, depth)
-    return (10.0 / depth) if board.winner == @symbol
+    return (10.0 / depth) if board.winner == symbol
     return (-10.0 / depth) if board.winner == opponent_symbol
     return 0 if board.full?
   end
