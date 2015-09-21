5d63bd94cfdc2703594830ca9c3a24bec03dc775
Nick Dyer <nbdyer@gmail.com>
2015-09-21 23:00:28 +0100

    Resolves errors in engine for change to dependencies relating to interface change

--- lib/engine.rb
+++ lib/engine.rb
@@ -1,10 +1,10 @@
 class Engine
 
-  attr_reader :game, :display
+  attr_reader :game, :ui
 
-  def initialize(game, display)
+  def initialize(game, ui)
     @game = game
-    @display = display
+    @ui = ui
   end
 
   def start
@@ -13,30 +13,30 @@ class Engine
   end
 
   def play_game
-    display.show(game.board)
+    ui.show(game.board)
     until game.ended? do
-      process_mark(game.turn.choose_move)
-      display.show(game.board)
+      process_mark(game.turn.choose_move(game.board))
+      ui.show(game.board)
     end
-    display.announce_winner(game.opponent) if game.has_a_winner?
-    display.announce_draw if game.draw?
+    ui.announce_winner(game.opponent) if game.has_a_winner?
+    ui.announce_draw if game.draw?
     another_round_query
   end
 
   def restart
     game.reset
-    display.introduction
+    ui.introduction
     play_game if game.ready?
   end
 
   def process_mark(position)
-    return display.bad_move unless game.valid_move?(position)
+    return ui.bad_move unless game.valid_move?(position)
     game.mark(position, game.turn)
   end
 
   def print_introduction
-    display.introduction
-    display.choose_game
+    ui.introduction
+    ui.choose_game
     case $stdin.gets.chomp
     when "1"
       setup_two_player_game
@@ -50,35 +50,34 @@ class Engine
   end
 
   def setup_two_player_game
-    2.times{ game.add_player(Player.new(display)) }
+    2.times{ game.add_player(Player.new(ui)) }
     [1,2].each { |player| assign_name(player) }
   end
 
   def setup_one_player_game
-    [Player.new(display), Computer.new].each { |player| game.add_player(player) }
-    game.player2.name, game.player2.engine = "Tron", self
+    [Player.new(ui), Computer.new(ui)].each { |player| game.add_player(player) }
+    game.player2.name = "Tron" 
     assign_name(1)
   end
 
   def setup_ai_game
-    2.times{ game.add_player(Computer.new) }
+    2.times{ game.add_player(Computer.new(ui)) }
     game.player1.name, game.player2.name = "Tron", "Hal 9000"
-    game.player1.engine, game.player2.engine = self, self
   end
 
   def assign_symbol(player)
-    display.mark_query(player)
+    ui.mark_query(player)
     game.player1.symbol = $stdin.gets.chomp == "1" ? "X" : "O"
     game.player2.symbol = game.player1.symbol == "O" ? "X" : "O"
   end
 
   def assign_first_player(player)
-    display.starting_player_query(player)
+    ui.starting_player_query(player)
     game.turn = $stdin.gets.chomp == "y" ? player : game.opponent
   end
 
   def assign_name(player_number)
-    display.name_query(player_number)
+    ui.name_query(player_number)
     game.public_send("player#{player_number}").name = $stdin.gets.chomp
   end
 
