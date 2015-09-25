4bed8f6ec90dad99aa6b85741c59496bc53f79db
Nick Dyer <nbdyer@gmail.com>
2015-09-25 20:24:44 +0100

    Tests computer choose_move method

--- spec/computer_spec.rb
+++ spec/computer_spec.rb
@@ -35,48 +35,52 @@ describe TicTacToe::Computer do
     expect(computer.score(draw_board, 0)).to eq 0
   end
 
-  context "during a game" do
+  it "will choose a move" do
+    expect(ui).to receive(:computer_move)
+    expect(computer).to receive(:chosen_move).with(board)
+    computer.choose_move(board)
+  end
 
-    it "will choose the a corner if playing first" do
-      expect(computer.chosen_move(board)).to satisfy("be a corner") do |num|
-        [0, 2, 6, 8].include? num
-      end
-    end
-
-    it "will play center if the first player took a corner" do
-      board = TicTacToe::Board.new({ :grid => ["O", 2, 3, 4, 5, 6, 7, 8, 9] })
-      expect(computer.chosen_move(board)).to eq 4
-    end
-
-    it "will win game if option is available" do
-      board = TicTacToe::Board.new({ :grid => ["X", 2, "X", 4, 5, 6, 7, 8, 9] })
-      expect(computer.chosen_move(board)).to eq 1
-    end
-
-    it "will stop win game if option is available" do
-      board = TicTacToe::Board.new({ :grid => [1, "O", 3, 4, 5, "O", "X", "X", "O"] })
-      expect(computer.chosen_move(board)).to eq 2
-    end
-
-    it "will prevent diagonal fork" do
-      board = TicTacToe::Board.new({ :grid => ["X", 2, 3, 4, "O", 6, 7, 8, "O"] })
-      expect(computer.chosen_move(board)).to eq 2
-    end
-
-    it "will prevent another diagonal fork" do
-      board = TicTacToe::Board.new({ :grid => ["O", 2, 3, 4, 5, "X", 7, 8, "O"] })
-      expect(computer.chosen_move(board)).to eq 4
-    end
-
-    it "will prevent edge trap" do
-      board = TicTacToe::Board.new({ :grid => [1, "O", 3, "O", "X", 6, 7, 8, 9] })
-      expect(computer.chosen_move(board)).to eq 0
-    end
-
-    it "will prevent reverse edge trap" do
-      board = TicTacToe::Board.new({ :grid => [1, 2, 3, 4, "X", "O", 7, "O", 9] })
-      expect(computer.chosen_move(board)).to eq 2
+  it "will choose the a corner if playing first" do
+    expect(computer.chosen_move(board)).to satisfy("be a corner") do |num|
+      [0, 2, 6, 8].include? num
     end
   end
 
+  it "will play center if the first player took a corner" do
+    board = TicTacToe::Board.new({ :grid => ["O", 2, 3, 4, 5, 6, 7, 8, 9] })
+    expect(computer.chosen_move(board)).to eq 4
+  end
+
+  it "will win game if option is available" do
+    board = TicTacToe::Board.new({ :grid => ["X", 2, "X", 4, 5, 6, 7, 8, 9] })
+    expect(computer.chosen_move(board)).to eq 1
+  end
+
+  it "will stop win game if option is available" do
+    board = TicTacToe::Board.new({ :grid => [1, "O", 3, 4, 5, "O", "X", "X", "O"] })
+    expect(computer.chosen_move(board)).to eq 2
+  end
+
+  it "will prevent diagonal fork" do
+    board = TicTacToe::Board.new({ :grid => ["X", 2, 3, 4, "O", 6, 7, 8, "O"] })
+    expect(computer.chosen_move(board)).to eq 2
+  end
+
+  it "will prevent another diagonal fork" do
+    board = TicTacToe::Board.new({ :grid => ["O", 2, 3, 4, 5, "X", 7, 8, "O"] })
+    expect(computer.chosen_move(board)).to eq 4
+  end
+
+  it "will prevent edge trap" do
+    board = TicTacToe::Board.new({ :grid => [1, "O", 3, "O", "X", 6, 7, 8, 9] })
+    expect(computer.chosen_move(board)).to eq 0
+  end
+
+  it "will prevent reverse edge trap" do
+    board = TicTacToe::Board.new({ :grid => [1, 2, 3, 4, "X", "O", 7, "O", 9] })
+    expect(computer.chosen_move(board)).to eq 2
+  end
+
 end
+
