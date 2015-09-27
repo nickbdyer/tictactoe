de82e91c737c2df044932bc6e7a73e7c6b0a6d72
Nick Dyer <nbdyer@gmail.com>
2015-09-27 14:50:18 +0100

    Refactor input validation on user_interface with a lambda no less

--- lib/game.rb
+++ lib/game.rb
@@ -2,7 +2,7 @@ require_relative 'player'
 require_relative 'computer'
 
 module TicTacToe
-  #The game class is responsible for tictactoe logic, and player flow"
+  #The game class is responsible for tictactoe logic, and player management.
   class Game
 
     attr_reader :player1, :player2, :board
--- lib/user_interface.rb
+++ lib/user_interface.rb
@@ -13,6 +13,14 @@ module TicTacToe
       @input, @output = input, output
     end
 
+    def introduction
+      output.puts CLEAR_SCREEN
+      output.puts STARS
+      output.puts TITLE
+      output.puts STARS
+      output.puts " "
+    end
+
     def show(board)
       output.puts CLEAR_SCREEN
       display_board = board.size == 9 ? GRIDx3.dup : GRIDx4.dup
@@ -23,23 +31,50 @@ module TicTacToe
       output.puts display_board
     end
 
-    def introduction
-      output.puts CLEAR_SCREEN
-      output.puts STARS
-      output.puts TITLE
-      output.puts STARS
-      output.puts " "
-    end
-
     def choose_game
       output.puts GAME_TYPE_CHOICE
-      selection = input.gets.chomp
-      until ["1", "2", "3"].include? selection do
-        try_choose_game_again
-        selection = input.gets.chomp
-      end
-      selection
+      validate_input(input.gets.chomp, ["1", "2", "3"], method(:try_choose_game_again))
     end
+
+    def name_query(number)
+      output.puts "Player #{number}, what is your name?"
+      input.gets.chomp
+    end
+
+    def mark_query(player)
+      output.puts "#{player.name}, what mark would you like to play as?"
+      output.puts MARK_CHOICE
+      validate_input(input.gets.chomp, ["1", "2"], method(:try_choose_mark_again))
+    end
+
+    def human_move(board, player)
+      output.puts "#{player.name}, it's your move, choose a cell by selecting a number."
+      selection = validate_input(input.gets.chomp.to_i - 1, 
+                                 board.available_cells, 
+                                 method(:try_choose_move_again),
+                                 true)
+      board.mark(selection, player.symbol)
+    end
+
+    def computer_move(board, move, computer)
+      output.puts "#{computer.name} has made a move"
+      board.mark(move, computer.symbol)
+    end
+
+    def announce_winner(player)
+      output.puts "#{player.name} is the winner."
+    end
+
+    def announce_draw
+      output.puts "It is a draw!"
+    end
+
+    def another_round?
+      output.puts "If you'd like to play again type 'y', or any other letter to quit."
+      input.gets.chomp == "y"
+    end
+
+    private 
 
     def try_choose_game_again
       output.puts INSULT
@@ -56,52 +91,15 @@ module TicTacToe
     def try_choose_move_again
       output.puts ILLEGAL_MOVE
       output.puts STARS
+      output.puts "Choose a cell by selecting a number that is shown on the board."
     end
 
-    def name_query(number)
-      output.puts "Player #{number}, what is your name?"
-      input.gets.chomp
-    end
-
-    def mark_query(player)
-      output.puts "#{player.name}, what mark would you like to play as?"
-      output.puts MARK_CHOICE
-      selection = input.gets.chomp
-      until ["1", "2"].include? selection do
-        try_choose_mark_again
-        selection = input.gets.chomp
+    def validate_input(user_input, options, response, zero_indexed = false)
+      until options.include? user_input do
+        response.call
+        user_input = zero_indexed ? input.gets.chomp.to_i - 1 : input.gets.chomp
       end
-      selection
-    end
-
-    def human_move(board, player)
-      output.puts "#{player.name}, it's your move, choose a cell by selecting a number."
-      selection = input.gets.chomp.to_i - 1
-      until board.available_cells.include? selection do
-        try_choose_move_again
-        output.puts "#{player.name}, it's your move, choose a cell by selecting a number."
-        selection = input.gets.chomp.to_i - 1
-      end
-      board.mark(selection, player.symbol)
-    end
-
-    def computer_move(board, move, computer)
-      output.puts "#{computer.name} has made a move"
-      board.mark(move, computer.symbol)
-    end
-
-    def announce_winner(player)
-      output.puts "#{player.name} is the winner."
-      output.puts "If you'd like to play again type 'y', or any other letter to quit."
-    end
-
-    def announce_draw
-      output.puts "It is a draw!"
-      output.puts "If you'd like to play again type 'y', or any other letter to quit."
-    end
-
-    def another_round?
-      input.gets.chomp == "y"
+      user_input
     end
 
   end
